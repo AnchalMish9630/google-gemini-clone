42223b9e23513e32a2e3e624834624fbaf80d60e
AnchalMish9630 <anchalmish2025@gmail.com>
2025-11-01 23:28:35 +0530

    App file

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sidebar from './component/sidebar/sidebar'
+import MainContainer from './component/main-container/mainContainer';
+
+export const App = () => {
+  return (
+    // <div>
+    <>
+      <Sidebar />
+      <MainContainer />
+
+    {/* // </div> */}
+    </>
+  )
+}
+
+export default App;
