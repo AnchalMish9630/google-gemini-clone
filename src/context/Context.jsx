f299dedff9e3c02734286d52745dc4210b15ae53
AnchalMish9630 <anchalmish2025@gmail.com>
2025-11-07 23:24:52 +0530

    Context changes

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -7,14 +7,27 @@ export const Context = createContext();
 const ContextProvider = ({ children }) => {
   const [result, setResult] = useState("");
 
+  const [input, setInput] = useState("")
+  const [recentPrompt, setRecentPrompt] = useState("")
+  const [previousPrompts, setPreviousPrompts] = useState("")
+  const [showResult, setShowResult] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [resultData, setRresultData] = useState("")
+
   // onSent returns the response (so callers can await it)
   const onSent = async (prompt) => {
-    if (!prompt) return "";
+    if (!input) return "";
     try {
-      // optional: set a temporary state while waiting
-      setResult("Thinking...");
-      const response = await runChat(prompt);
-      setResult(response);
+      // // optional: set a temporary state while waiting
+      // setResult("Thinking...");
+      setRresultData("");
+      setLoading(true)
+      setRecentPrompt(input)
+      setShowResult(true)
+      const response = await runChat(input);
+      setRresultData(response);
+      setLoading(false);
+      setInput("");
       return response; // important: return the text so useEffect can log it
     } catch (err) {
       setResult("Error: " + (err?.message ?? "unknown"));
@@ -31,7 +44,20 @@ const ContextProvider = ({ children }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const contextValue = { onSent, result };
+  const contextValue = { onSent, result , 
+    input, 
+    setInput,
+    resultData,
+    setRresultData,
+    recentPrompt,
+    setRecentPrompt,
+    previousPrompts,
+    setPreviousPrompts,
+    setLoading,
+    loading,
+    showResult,
+    setShowResult
+  };
 
   return <Context.Provider value={contextValue}>{children}</Context.Provider>;
 };
